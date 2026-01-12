8e64ebbf37420170db49bea0cd58cac992499fb1
MindFind <mindfindbox@gmail.com>
2026-01-13 01:37:37 +0300

    Update imdbrt.js

--- imdbrt.js
+++ imdbrt.js
@@ -77,14 +77,28 @@
                     background: '#c2410c',
                     color: '#fff',
                     fontFamily: '',
-                    fontSize: '',
-                    lineHeight: '',
+                    fontSize: '1.45em',          // масштаб относительно блока IMDb
+                    fontWeight: 'semibold',
+                    lineHeight: '1.45em',        // вертикальный «воздух»
                     padding: '0 0.5em',
                     borderRadius: '0.3em',
                     display: 'inline-flex',
                     alignItems: 'center',
                     justifyContent: 'center',
                     marginRight: '0.5em'
+
+                    fontSize: '1.45em',          // масштаб относительно блока IMDb
+                    fontWeight: 'semibold',
+                    lineHeight: '1.45em',        // вертикальный «воздух»
+                    padding: '0 0.5em',     // сверху/снизу/слева/справа
+                    borderRadius: '0.23em',
+                    boxSizing: 'border-box',
+                    marginRight: '0.5em',
+                    display: 'inline-flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+
+                    
                 });
 
             $(imdbEl).before(votesBox);
